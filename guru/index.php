29bfa431df773117f47ad332f69665f8a0134e7c
sovirau <99818943+sovirau@users.noreply.github.com>
2024-04-22 11:12:42 +0700

    unzipgit

---
+++ guru/index.php
@@ -0,0 +1,100 @@
+<?php
+error_reporting(0);
+session_start();
+if(!isset($_SESSION['username'])){
+  header("location:../");
+}
+require_once ('config.php');
+$kode_guru = $_SESSION['username'];
+$guru = mysql_query("SELECT a.*, b.* from tb_guru a 
+                    inner join tb_login b on a.kode_guru = b.kode_user
+                    where b.username = '$kode_guru'");
+$gururow = mysql_fetch_array($guru);
+$id = $gururow['kode_guru'];
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Nilai Akademik</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME ICONS  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <link href="assets/css/bootstrap-select.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                        <a class="navbar-brand" href="#" style="color:white">Nilai Akademik SKARIGA</a>
+                </div>
+                <div class="col-md-6">
+                    <strong>Selamat Datang, </strong><?php echo $_SESSION['username']; ?> &nbsp;
+            <!-- Top Menu Items -->
+            <ul class="nav navbar-right top-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color:white"><i class="fa fa-user"></i><b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="?pages=edit_profil&kode_guru=<?php echo $id; ?>"><i class="fa fa-fw fa-user"></i> Profile</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="logout.php"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    <!-- HEADER END-->
+    <!-- LOGO HEADER END-->
+    <section class="menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="?pages=dashboard">Dashboard</a></li>
+                            <li><a href="?pages=data_kompetensi">Kompetensi</a></li>
+                            <li><a href="?pages=nilai">Nilai</a></li> 
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- MENU SECTION END--><?php require_once('pages.php'); ?>
+    <!-- FOOTER SECTION END--><?php require_once('footer.php'); ?>
+    <!-- JAVASCRIPT AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.11.1.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/bootstrap-select.js"></script>
+</body>
+</html>
