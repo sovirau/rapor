1abd34fbb9d495ed2e9ac46954e103da9a59e20f
sovirau <99818943+sovirau@users.noreply.github.com>
2024-04-22 11:13:51 +0700

    unzipgit

---
+++ siswa/footer.php
@@ -0,0 +1,11 @@
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2015 Sovira Fitra Aulia 3RPA</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
